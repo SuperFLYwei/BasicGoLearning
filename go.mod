883f5111139e5e8683eb67651d1e1a0f19c0894a
Akmf <763941102@qq.com>
2024-06-21 17:16:25 +0800

    data struct

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/SuperFLYwei/BasicGoLearning
+
+go 1.22.4
+
+require (
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
---
+++ msgpack/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+
+	"github.com/vmihailenco/msgpack"
+)
+
+type Person struct {
+	Name string
+	Age  int
+	Sex  string
+}
+
+// 二进制写出
+func writerJson(filename string) (err error) {
+	var persons []*Person
+	// 假数据
+	for i := 0; i < 10; i++ {
+		p := &Person{
+			Name: fmt.Sprintf("name%d", i),
+			Age:  rand.Intn(100),
+			Sex:  "male",
+		}
+		persons = append(persons, p)
+	}
+	// 二进制json序列化
+	data, err := msgpack.Marshal(persons)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = ioutil.WriteFile(filename, data, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	return
+}
+
+// 二进制读取
+func readJson(filename string) (err error) {
+	var persons []*Person
+	// 读文件
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// 反序列化
+	err = msgpack.Unmarshal(data, &persons)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, v := range persons {
+		fmt.Printf("%#v\n", v)
+	}
+	return
+}
+
+func main() {
+	err := writerJson("./person.dat")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
